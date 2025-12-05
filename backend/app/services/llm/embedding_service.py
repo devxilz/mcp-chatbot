3cbffe962cd7557700fde31060a0a749abf04d7c
devxilz <devxilz777@gmail.com>
2025-12-06 00:28:23 +0530

    Your feature or fix

--- backend/app/services/llm/embedding_service.py
+++ backend/app/services/llm/embedding_service.py
@@ -1,5 +1,6 @@
 from app.utils.model_loder import get_model
 
+# Function to generate embeddings using the loaded model
 def generate_embedding(text: str):
     model = get_model()
     embedding = model.encode(text, convert_to_tensor=False)
